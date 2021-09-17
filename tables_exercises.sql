7f3fe9b1729488123bd9dd7e0a2d2629131c341f
Brian Clements <brian.l.clements03@gmail.com>
2021-09-17 14:38:43 -0500

    add new notes to tables_exercises

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,6 +1,16 @@
 show databases;
+-- Use the employees database
 use employees;
+-- List all the tables in the database
 show tables;
+-- Explore the employees table. What different data types are present on this table?
 describe employees;
+-- Which table(s) do you think contain a numeric type column?
+-- Which table(s) do you think contain a string type column?
+-- Which table(s) do you think contain a date type column?
+
+-- What is the relationship between the employees and the departments tables? SEE dept_emp TABLE! CROSS REFERENCE
 describe departments;
+describe employees;
+-- Show the SQL that created the dept_manager table.
 SHOW CREATE TABLE `dept_manager`;
