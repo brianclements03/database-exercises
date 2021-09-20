744ef97a2f4a6ebc44acb4177b2002c7339f5e6d
Brian Clements <brian.l.clements03@gmail.com>
2021-09-20 12:25:53 -0500

    change file name and add to order_by_and_limit_notes

--- order_by_and_limit_notes.sql
+++ order_by_and_limit_notes.sql
@@ -20,3 +20,11 @@ SELECT NULL = 2 --------> NULL
 		
 SELECT * FROM employees WHERE first_name LIKE 'eb' AND NOT first_name LIKE '%e';
 SELECT * FROM logs WHERE cohort_id IS NOT NULL AND user_id = 40;
+
+
+-- -------------------LIMIT NOTES-----------------------------
+-- The limit clauses limits results to a range or a number
+SELECT emp_no, first_name, last_name FROM employees WHERE first_name LIKE 'M%' LIMIT 10;
+-- OFFSET tells it where to start from
+SELECT emp_no, first_name, last_name FROM employees WHERE first_name LIKE 'M%' LIMIT 25 OFFSET 50;
+-- commonly used for "pagination"
