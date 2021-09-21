7d22ff958e46a17fb9f82aaf1408c11149727842
Brian Clements <brian.l.clements03@gmail.com>
2021-09-21 13:57:49 -0500

    complete function exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -23,3 +23,16 @@ USE employees;
 SELECT last_name FROM employees WHERE last_name LIKE 'e%e';
 SELECT concat(first_name, '', last_name) AS full_name FROM employees 
 WHERE last_name LIKE 'e%e';
+-- 3. ----Convert the names to uppercase
+SELECT upper(concat(first_name, '', last_name)) AS full_name FROM employees 
+WHERE last_name LIKE 'e%e';
+-- 4. ----Find all emp hired in 90s AND born on xmas. Use datediff() to find how many days they've been at the company
+SELECT datediff(curdate(), hire_date) FROM employees WHERE hire_date LIKE '199%' 
+AND birth_date LIKE '%-12-25';
+-- 5. ----Find min and max salary from salaries table
+SELECT min(salary), max(salary) FROM salaries;
+-- 6. ----Generate a username for all emp. All lowercase, 1st char of 
+-- first_name, 1st 4 char last_name, "_", birth month, last 2 digs of birth year
+SELECT lower(concat(substr(first_name,1,1), substr(last_name,1,4), '_', 
+substr(birth_date,6,2), substr(birth_date,3,2))) AS user_name FROM employees;
+SELECT substr(birth_date,3,2) FROM employees;
