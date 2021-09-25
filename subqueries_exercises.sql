454b3f12b8d55e70d09849b4e260a462e04201fa
Brian Clements <brian.l.clements03@gmail.com>
2021-09-25 11:16:03 -0500

    update subqueries exercises

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,3 +1,4 @@
+USE employees;
 -- 1.Find all the current employees with the same hire date as employee 101010 using a sub-query.
 SELECT * 
 FROM employees
@@ -19,4 +20,9 @@ AND first_name = (
 -- 3.How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
 SELECT * #COUNT(*)
 FROM employees e
-JOIN dept_emp de using(emp_no)
+WHERE emp_no IN (
+	SELECT emp_no
+	FROM dept_emp
+    WHERE to_date < now()
+    );
+-- 4.Find all the current department managers that are female. List their names in a comment in your code.
