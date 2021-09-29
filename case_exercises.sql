0fce740590fa7e961b150c422a54eaac81c20803
Brian Clements <brian.l.clements03@gmail.com>
2021-09-29 12:48:29 -0500

    add bonus solution

--- case_exercises.sql
+++ case_exercises.sql
@@ -40,14 +40,53 @@ SELECT first_name, last_name,
 		ELSE 'R-Z' 
 		END AS alpha_group
 FROM employees;
-
+-- OOOORRRRRR----
+select first_name, last_name,
+    case
+    when last_name < 'I' then 'A-H'
+    when last_name < 'R' then 'I-Q'
+    else 'R-Z'
+    end as alpha_group
+from employees;
 -- 3.How many employees (current or previous) were born in each decade?
-SELECT birth_date,
+SELECT COUNT(*),
 CASE 
 	WHEN birth_date BETWEEN '1950-01-01' AND '1959-12-31' THEN 'fifties'
     WHEN birth_date BETWEEN '1960-01-01' AND '1969-12-31' THEN 'sixties'
     ELSE NULL
     END AS Birth_Decade
-FROM employees;
+FROM employees
+GROUP BY Birth_Decade;
+
+-- BONUS. What is the current average salary for each of the following department groups: 
+-- R&D, Sales & Marketing, Prod & QM, Finance & HR, Customer Service?
+USE employees; SELECT dept_name FROM departments;
+
+SELECT 
+       CASE 
+           WHEN dept_name IN ('research', 'development') THEN 'R&D'
+           WHEN dept_name IN ('sales', 'marketing') THEN 'Sales & Marketing' 
+           WHEN dept_name IN ('Production', 'Quality Management') THEN 'Prod & QM'
+           WHEN dept_name IN ('Finance','Human Resources') THEN 'Finance & HR'
+           ELSE 'Customer Service'
+		END AS dept_group,
+			AVG(salary) AS average_salary
+FROM salaries
+JOIN dept_emp USING(emp_no)
+JOIN departments USING(dept_no)
+WHERE salaries.to_date > curdate()
+GROUP BY dept_group
+ORDER BY dept_group;
 
 
+
+-- dept_group avg sal is just avg(salary_dept1) + avg(salary_dept2)
+/*+-------------------+-----------------+
+| dept_group        | avg_salary      |
++-------------------+-----------------+
+| Customer Service  |                 |
+| Finance & HR      |                 |
+| Sales & Marketing |                 |
+| Prod & QM         |                 |
+| R&D               |                 |
++-------------------+-----------------+*/
